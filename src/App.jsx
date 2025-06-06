98a481b0776d8e4912520f361d25ac2e2fbf16ef
Sparkup IT Academy <sparkupitacademy@gmail.com>
2025-06-06 15:26:56 +0545

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+
 export default function App() {
   return (
-    <div>App</div>
+    <Routes>
+        <Route path='/' element={<IndexPage/>}></Route>
+        <Route path='/home' element={<HomePage/>}></Route>
+        <Route path='/login' element={<LoginPage />}></Route>
+    </Routes>
   )
 }
