3fcaf754be66fe31e78a3757204c5b0e7fe5a664
Sparkup IT Academy <sparkupitacademy@gmail.com>
2025-06-13 14:41:00 +0545

    feat: useCallback hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, NavLink } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Reducer from './pages/Reducer';
 import Memo from './pages/Memo';
+import Callback from './pages/Callback';
 
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
               <Route path='/' element={<HomePage/>}></Route>
               <Route path='/reducer' element={<Reducer/>}></Route>
               <Route path='/memo' element={<Memo />}></Route>
+              <Route path='/callback' element={<Callback />}></Route>
           </Routes> 
       </div>
     </>
